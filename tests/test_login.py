33ac866140a84dd4565243ac38ca193fef51a6f0
Morolong <mg.morolong@gmail.com>
2026-09-15 17:22:10 +0200

    test(login): use registered user fixture for valid login test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,14 +1,15 @@
 from pages.login_page import LoginPage
-from playwright.sync_api import expect 
-from test_data.users import VALID_USER
+from playwright.sync_api import expect
 
-def test_valid_login(page): 
+def test_valid_login(page, registered_user):
+    username = registered_user["username"]
+    password = registered_user["password"]
+
     login_page = LoginPage(page)
 
-    login_page.goto()
+    login_page.log_out()
 
-    login_page.login(VALID_USER["username"], 
-                     VALID_USER["password"]
-                     )
-    
+    login_page.goto()
+    login_page.login(username, password)
+
     expect(page.get_by_role("heading", name="Accounts Overview")).to_be_visible()
